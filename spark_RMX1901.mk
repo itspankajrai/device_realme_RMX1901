88d31d700a25a7a2af8481767f3905146502e05d
Shravankumar <shravankumarsayz@gmail.com>
2023-09-22 09:08:24 +0100

    RMX1901: Opt out of EPPE

--- spark_RMX1901.mk
+++ spark_RMX1901.mk
@@ -9,6 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Spark stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
