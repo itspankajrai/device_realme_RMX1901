d600dd85db6528ad8d3e2379a817bb48dce599d2
Shravankumar <shravankumarsayz@gmail.com>
2023-09-24 06:11:53 +0100

    RMX1901: Voltage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_RMX1901.mk
+    $(LOCAL_DIR)/voltage_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_RMX1901-userdebug \
-    spark_RMX1901-eng
+    voltage_RMX1901-userdebug \
+    voltage_RMX1901-eng
 
